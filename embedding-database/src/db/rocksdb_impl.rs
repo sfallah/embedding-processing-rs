8a586ca7037d10b8ee11f4cac9b2f7b17158a41e
Anil Aydiner <>
2024-11-12 15:55:49 +0300

    Use futures on async db writes, adjust dao to have db reference, introduce args for cli and clean unused imports

--- embedding-database/src/db/rocksdb_impl.rs
+++ embedding-database/src/db/rocksdb_impl.rs
@@ -5,9 +5,8 @@ use rocksdb::{
 };
 use std::sync::Arc;
 use anyhow::{Result, anyhow};
-use serde::{Deserialize, Serialize};
 use tokio::task;
-use tracing::{info, error};
+use tracing::info;
 use tracing::instrument;
 use crate::db::column_families::ColumnFamilyType;
 
