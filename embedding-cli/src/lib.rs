2fbc2230fb1e6fec542d36318447e3d88f0f91e7
Saba Fallah <>
2024-11-19 18:11:44 +0100

    first draft of cli-query

--- embedding-cli/src/lib.rs
+++ embedding-cli/src/lib.rs
@@ -1,19 +1,15 @@
 pub mod file_io;
 
-use clap::ValueEnum;
+use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
-
-#[derive(clap::Parser, Debug, Clone)]
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
 pub struct Cli {
-
     /// Logging level (trace, debug, info, warn, error)
     #[arg(long, default_value = "info", value_enum)]
     pub log_level: tracing::Level,
-
-    /// Path to the text file to process
-    #[arg(long, default_value = "embedding-processing/tests/test_data/")]
-    pub file_path: PathBuf,
 
     #[arg(long)]
     pub user_id: Option<String>,
@@ -21,14 +17,39 @@ pub struct Cli {
     #[arg(long, default_value = "config.toml")]
     pub config_file: String,
 
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Commands {
+    /// Adds files to myapp
+    Index {
+        /// Path to the text file to process
+        #[arg(long, default_value = "embedding-processing/tests/test_data/")]
+        file_path: PathBuf,
+    },
+    Query {
+        /// Path to the text file to process
+        #[arg(long)]
+        query: String,
+
+        #[arg(long, default_value = "10")]
+        top_k: usize,
+    },
 }
 
 impl Cli {
     pub fn validate(&self) -> Result<(), String> {
-        if self.file_path.is_dir() {
-            Ok(())
-        } else {
-            Err(format!("{} is not a directory", self.file_path.display()))
+        match self.command {
+            Commands::Index { ref file_path } => {
+                if file_path.is_dir() {
+                    Ok(())
+                } else {
+                    Err(format!("{} is not a directory", file_path.display()))
+                }
+            }
+            Commands::Query { .. } => Ok(()),
         }
     }
 }
