f6a53d4ce9404da227f4a2713f825a6be25f4476
sfallah <>
2024-12-18 17:31:38 +0100

    running now

--- .devops/starter.sh
+++ .devops/starter.sh
@@ -10,5 +10,4 @@ echo $LD_LIBRARY_PATH
 ls -la ${LLAMA_PATH}/lib/
 ls -la ${LLAMA_PATH}/include/
 ls -la ${LLAMA_PATH}/bin/
-/usr/src/app/target/release/embedding-server --help 
-#--config-file /usr/src/app/config/config.toml --user-id "f1143e1a-828b-4e9b-8ac8-00c8cbd8cefe"
+/usr/src/app/target/release/embedding-server --config-file /usr/src/app/config/config.toml --user-id "f1143e1a-828b-4e9b-8ac8-00c8cbd8cefe"
