4fdd289183b63f088472355e83bb44fefc4e1da4
me <>
2024-12-18 05:16:20 +0100

    removed the planner

--- .devops/starter.sh
+++ .devops/starter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 echo "Starting embedding server"
-ls -la /usr/local/bin/embedding-cli
+ls -lah /usr/src/app/target/release/
 nvidia-smi
 echo "llama path"
 echo $LLAMA_PATH
@@ -10,5 +10,5 @@ echo $LD_LIBRARY_PATH
 ls -la ${LLAMA_PATH}/lib/
 ls -la ${LLAMA_PATH}/include/
 ls -la ${LLAMA_PATH}/bin/
-/usr/local/bin/embedding-cli --help 
+/usr/src/app/target/release/embedding-server --help 
 #--config-file /usr/src/app/config/config.toml --user-id "f1143e1a-828b-4e9b-8ac8-00c8cbd8cefe"
