8c8a24cb2adc68e27888e8cd89c83873a2635f75
Saba Fallah <>
2025-02-10 18:11:20 +0100

    minor change

--- embedding-model/src/model_worker.rs
+++ embedding-model/src/model_worker.rs
@@ -38,7 +38,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     // offload all layers to the gpu
-    let model_params = if cfg!(feature = "cuda") || cfg!(feature = "vulkan") {
+    let model_params = if cfg!(feature = "cuda") {
         LlamaModelParams::default().with_n_gpu_layers(config.model_config.ngl as u32)
     } else {
         LlamaModelParams::default()
