1477e14908df467f69a12ca4be14fb72f33bb735
Saba Fallah <>
2025-02-17 09:03:36 +0100

    testing horizontal scalability

--- embedding-processing/benches/benchmarks.rs
+++ embedding-processing/benches/benchmarks.rs
@@ -102,12 +102,12 @@ pub fn benches() {
         .measurement_time(std::time::Duration::from_secs(20))
         .configure_from_args();
 
-    //let file_path = "tests/test_data/superlinear.txt".to_string();
-    let file_path = "tests/test_data/United_States.txt".to_string();
+    let file_path = "tests/test_data/superlinear.txt".to_string();
+    //let file_path = "tests/test_data/United_States.txt".to_string();
     // read the file
     let doc = fs::read_to_string(file_path).unwrap();
     process_doc(&mut criterion, doc.clone());
     embedding_benchmark(&mut criterion, doc.clone());
-    embedding_benchmark_batch(&mut criterion, doc);
+    //embedding_benchmark_batch(&mut criterion, doc);
 }
 criterion_main!(benches);
