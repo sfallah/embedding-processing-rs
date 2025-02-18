802c6b472dcf1e07c84613485286e80f700a6bc2
Saba Fallah <>
2025-02-18 21:56:25 +0100

    doc processing bench

--- embedding-processing/src/processing/embeddings.rs
+++ embedding-processing/src/processing/embeddings.rs
@@ -34,34 +34,3 @@ pub fn get_embeddings(
     let response: EmbeddingsResponse = EmbeddingsResponse::unpack(&rsp).expect("Failed to unpack");
     Ok(response.embeddings)
 }
-
-pub fn get_embeddings_2(
-    zmq_ctx: Arc<zmq::Context>,
-    model_endpoint: &str,
-    n_embd: usize,
-    texts: &[String],
-    embed_id: u64,
-) -> anyhow::Result<Vec<Vec<f32>>> {
-    let socket = zmq_ctx.socket(zmq::DEALER)?;
-    socket.connect(&model_endpoint).expect("Failed to connect");
-    socket.set_linger(0).expect("Failed to set linger");
-    socket
-        .set_sndtimeo(1000)
-        .expect("Failed to set send timeout");
-    socket
-        .set_rcvtimeo(30000)
-        .expect("Failed to set receive timeout");
-    // identity random uuid
-    //let identity = uuid::Uuid::new_v4();
-    socket
-        .set_identity(embed_id.to_string().as_bytes())
-        .expect("Failed to set identity");
-    trace!("Processing embedding...");
-    //FIXME: n_embd is hardcoded to 384
-    let request = EmbeddingsRequest::new(0, 0, n_embd, texts.to_vec());
-    let msg = request.pack().expect("Failed to pack");
-    socket.send(msg, 0).expect("Failed to send");
-    let rsp = socket.recv_bytes(0).expect("Failed to receive");
-    let response: EmbeddingsResponse = EmbeddingsResponse::unpack(&rsp).expect("Failed to unpack");
-    Ok(response.embeddings)
-}
